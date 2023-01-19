8434bdb418f9be73e0d2c175cba6ab002215929b
annavlasand <anvlas1996@yandex.ru>
2023-01-19 20:40:35 +0300

    data preprocessing

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ events_data.groupby('day') \
     .user_id.nunique() \
     .plot() #в первом приближении проверяем данные на адекватность (визуально)
 
-events_data.pivot_table(index='user_id',
+users_events_data = events_data.pivot_table(index='user_id',
                         columns='action',
                         values='step_id',
                         aggfunc='count',
@@ -24,7 +24,7 @@ users_scores = submissions_data.pivot_table(index='user_id',
                         columns='submission_status',
                         values='step_id',
                         aggfunc='count',
-                        fill_value=0).reset_index() #смотрим, сколько было правильных и неправильных сабмитов
+                        fill_value=0).reset_index() #смотрим попытки и баллы юзеров
 
 #так как нам нужно понять, кого именно считать покинувшими курс - тех, кто не появлялся на курсе 2 недели, месяц и тд,
 # проводим небольшие дополнительные исследования
@@ -35,5 +35,22 @@ gap_data = events_data[['user_id', 'day', 'timestamp']].drop_duplicates(subset=[
     .apply(np.diff).values
 gap_data = pd.Series(np.concatenate(gap_data, axis=0))
 gap_data = gap_data / (24 * 60 * 60)
-quant_95 = gap_data.quantile(0.95)  # 5 % юзеров возвращаются на курс после примерно 2х месяцев
-quant_90 = gap_data.quantile(0.9)   # 10 % юзеров возвращаются на курс после примерно 18 дней
+quant_95 = gap_data.quantile(0.95)  #5 % юзеров возвращаются на курс после примерно 2х месяцев
+quant_90 = gap_data.quantile(0.9)   #10 % юзеров возвращаются на курс после примерно 18 дней
+
+#возьмем приблизительно среднее - 30 дней и решим, что юзер считается покинувшим курс, если он не получил сертификат
+#и не появлялся на курсе больше 30 дней
+
+now = 1526772811 #день выгрузки данных
+drop_out_threshold = 2592000 #30 дней
+users_data = events_data.groupby('user_id', as_index=False) \
+    .agg({'timestamp': 'max'}).rename(columns={'timestamp': 'last_timestamp'})
+users_data['is_gone_user'] = (now - users_data.last_timestamp) > drop_out_threshold #условие для вылета с курса по отсутствию
+users_data = users_data.merge(users_scores, on='user_id', how='outer')
+users_data = users_data.fillna(0)
+users_data = users_data.merge(users_events_data, how='outer')
+users_days = events_data.groupby('user_id').day.nunique().to_frame().reset_index() #число уникальных дней для пользователей
+users_data = users_data.merge(users_days, how='outer')
+users_data.user_id.nunique() #проверка, не потеряли ли мы юзеров
+events_data.user_id.nunique() #проверка, не потеряли ли мы юзеров - все ок
+users_data['passed_corse'] = users_data.passed > 170 #условие окончания курса
